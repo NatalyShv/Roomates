947a6c965f5b1e0583339de28f599ac97d3eee53
OfekRv <ofekrv@gmail.com>
2018-10-25 15:10:00 +0300

    Update server.js

--- roomate/server.js
+++ roomate/server.js
@@ -1,3 +1,17 @@
-server.start(function () {
-  console.log('Server listening to', server.port);
-});
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
