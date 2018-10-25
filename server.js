c037a96a11dd1453f294f7b47eb4582a6a1dbd80
OfekRv <ofekrv@gmail.com>
2018-10-25 16:23:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ var path = require('path');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
+    console.log('V1');
+
+
 app.get('/', function(req, res) {
         console.log(__dirname);
     res.sendFile(path.join('roomate/src/app/app.component.html'));
 });
 
 app.get('/app', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join('roomate/src/app/app.component.html'));
 });
 
 connect().use(serveStatic('roomate/src')).listen(8080, function(){
