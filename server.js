e0bc5586eba0b739dc8624c952433e0341274bf8
OfekRv <ofekrv@gmail.com>
2018-10-25 15:14:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const http = require('http')
-const port = 8080
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
