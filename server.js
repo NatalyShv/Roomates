bc230eb0f0fee2fa5e76dd78f659e8aa3eff38cc
OfekRv <ofekrv@gmail.com>
2018-10-25 15:29:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+connect().use(serveStatic('roomate/src')).listen(8080, function(){
     console.log('Server running on 8080...');
 });
